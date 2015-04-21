bd360da1200b9289f5a933e7ac75cccbc8b81d60
Herre Groen <herregroen@gmail.com>
2015-04-21 16:36:53 +0200

    add support for associations

--- lib/sequel/simple_json.rb
+++ lib/sequel/simple_json.rb
@@ -14,17 +14,49 @@ module Sequel
             end
           end
         end
+        def json_associations *assocs
+          assocs.each do |assoc|
+            if assoc.respond_to?(:to_sym) and assoc = assoc.to_sym and self.associations.include?(assoc)
+              _json_assocs << assoc
+            end
+          end
+        end
         def _json_props
           @json_props ||= []
+        end
+        def _json_assocs
+          @json_assocs ||= []
         end
       end
       module DatasetMethods
         def to_json opts={}
-          if self.model._json_props.any?
-            self.select(*self.model._json_props).map(&:values).to_json
-          else
-            self.map(&:values).to_json
+          ds = self
+          self.model._json_assocs.each do |assoc|
+            r  = ds.model.association_reflection(assoc)
+            m  = r[:class_name].split('::').inject(Object) {|o,c| o.const_get c}
+            ds = ds.eager_graph(assoc => proc{|ads| ads.select(m.primay_key) })
           end
+          if self.model._json_attrs.any?
+            s  = self.model._json_attrs
+            s << self.model.primary_key unless s.include?(self.model.primary_key)
+            ds = ds.select{s.map{|c|`#{ds.model.table_name}.#{c}`}}
+          else
+            ds = ds.select{`#{ds.model.table_name}.*`}
+          end
+          g = (s || self.columns).map{|c| "#{ds.model.table_name}.#{c}"}
+          self.model._json_assocs.each do |assoc|
+            r = ds.model.association_reflection(assoc)
+            m = r[:class_name].split('::').inject(Object) {|o,c| o.const_get c}
+            if r[:cartesian_product_number] == 0
+              ds = ds.select_append{`\"#{assoc}\".\"#{m.primary_key}\"`.as(assoc)}
+              g << "\"#{assoc}\".\"#{m.primary_key}\""
+            else
+              ds = ds.select_append{array_agg(`\"#{assoc}\".\"#{m.primary_key}\"`).as(assoc)}
+            end
+          end
+          ds   = ds.group{g.map{|c| `#{c}`}}
+          json = ds.map(&:values).to_json
+          return json ? json.gsub(/\[null(\,\s?null)*\]/,'[]') : '[]'
         end
       end
       module InstanceMethods
